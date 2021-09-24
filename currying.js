59c08180cf9784e6f2a4bf2dbab13105a1317f54
Tulika Sharma <67149535+tulikasharma368@users.noreply.github.com>
2021-09-24 23:13:04 +0530

    [Tulika] added: volume example to illustrate currying

---
+++ currying.js
@@ -0,0 +1,13 @@
+function volume(length) {
+    return function(width) {
+       return function(height) {
+          console.log(length * width * height);
+       }
+    }
+ }
+volume(10)(20)(30);
+
+
+const res = volume(10);
+const res1 = res(20);
+const res2 = res1(30);
