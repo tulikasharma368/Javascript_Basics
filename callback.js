103a02920e07f2f3a5c8e3abd38f7245cb2e072c
Tulikasharma <tulikasharma368@gmail.com>
2021-09-24 23:04:26 +0530

    [Tulika]: resort example to illustrate callback

--- callback.js
+++ callback.js
@@ -1,26 +1,31 @@
-function A(value1, value2, callback){
-    console.log("this is callback function A");
-    callback(value1, value2);
+var readLine = require('readline');
+var RL = readLine.createInterface(process.stdin, process.stdout);
+
+function resort(name, location, callback){
+    console.log("Hey!");
+    callback(name, location);
 }
 
-function B(value1, callback){
-    console.log("This is callback function B");
-    callback(value1);
+function rooms(callback){
+    console.log("The available rooms are:");
+    callback(["Single room", "Double room", "Suite", "Dorm room"]);
 }
 
-function C(value1, value2, callback){
-    console.log("This is callback functon sum");
-    var sum = value1 + value2;
-    console.log("The sum is: " + sum);
-    callback(value1,value2);
+function message(num,callback){
+    console.log("\nMessage:");
+    callback(num);
 } 
 
-const practice = A("Tulika", "Sharma", (a,b) =>{
-    console.log(a,b);
-    B("IT",(a) => {
-        console.log(a);
-        C(10, 20, (c,d)=> {
-            console.log("Concat: " + c + d);//not working
+const practice = resort("Markson Resort", "Kalpa", (name, location) =>{
+    console.log("Heartiest welcome to the " + name + ", " + location + "\n");
+    rooms((arr) => {
+        arr.forEach(roomType => {
+            console.log(roomType);
         });
+        
+        message("9",(contact)=>{
+            console.log("Call at " + contact + " for anything. Enjoy your stay");
+        })
+
     });
 });
